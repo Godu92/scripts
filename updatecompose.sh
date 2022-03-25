1e892e384c930d5d2673696f0e1ddd6b6a224fef
Douglas Shoemaker <godu92@GoduGamer.localdomain>
2022-03-25 18:29:51 -0400

    making docker compose update script

---
+++ updatecompose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get remove docker-compose
+# curl + grep
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/"${VERSION}"/docker-compose-"$(uname -s)"-"$(uname -m)" -o $DESTINATION
+sudo chmod 755 "$DESTINATION"
